8220d31b83eeac1833603c7f9db0ea32dac4e133
Thomas Svartbäck <thomsva@gmail.com>
2019-02-01 23:04:28 +0200

    2.13 valmis

--- maidentiedot/src/index.js
+++ maidentiedot/src/index.js
@@ -10,7 +10,10 @@ const Country = ({ country }) => (
     <p>Population: {country.population}</p>
     <h2>Languages</h2>
     <ul>
-      {country.languages.map(language => <li>{language.name}</li>)}
+      {country.languages.map(language =>
+        <li key={language.name}>
+          {language.name}
+        </li>)}
     </ul>
     <img src={country.flag} alt='flag' width='200' ></img>
   </div>
@@ -18,17 +21,29 @@ const Country = ({ country }) => (
 
 
 
-const Countries = ({ countries, filter }) => {
+const Countries = ({ countries, filter, onSubmit, selected }) => {
   const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
-  if (filteredCountries.length === 1)
+  const n = filteredCountries.length
+  if (selected !== '')
+    return <Country country={countries.filter(country => country.alpha3Code === selected)[0]} />
+
+  if (n === 0)
+    return <p>No matches! Specify another filter.</p>
+  if (n === 1)
     return <Country country={filteredCountries[0]} />
-  if (filteredCountries.length <= 10)
-    return <ul> {filteredCountries.map(country =>
-      <li key={country.alpha3Code}>
-        {country.name} ({country.alpha3Code})
-    </li>)}</ul>
+  if (n <= 10)
+    return <div>
+      {filteredCountries.map(country =>
+        <form onSubmit={onSubmit} key={country.alpha3Code}>
+          {country.name}({country.alpha3Code})
+
+          <input type="hidden" name="code" value={country.alpha3Code} />
+          <button type="submit">Show</button>
+
+        </form>)
+      }</div>
   else
-    return <p>Too many matches ({filteredCountries.length}), specify another filter.</p>
+    return <p>Too many matches ({n}), specify another filter.</p>
 }
 
 const Filter = ({ filter, handleFilterChange }) =>
@@ -37,6 +52,7 @@ const Filter = ({ filter, handleFilterChange }) =>
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [selected, setSelected] = useState('')
 
   useEffect(() => {
     console.log('effect')
@@ -49,15 +65,25 @@ const App = () => {
   }, [])
 
   const handleFilterChange = (event) => {
+    setSelected('')
     setFilter(event.target.value)
   }
+
+  const handleSelect = (event) => {
+    event.preventDefault()
+    console.log('show button pressed', event.target)
+    setSelected(event.target.code.value)
+    console.log('Selected country:', selected)
+  }
+
+
 
 
   return (
     <div>
       <h1>Maiden tiedot</h1>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <Countries countries={countries} filter={filter} />
+      <Countries countries={countries} filter={filter} selected={selected} onSubmit={handleSelect} />
     </div>
   )
 }
