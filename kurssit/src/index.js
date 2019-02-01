a438b9569e79f535c93efeb2aed78910c72aaf08
Thomas Svartbäck <thomsva@gmail.com>
2019-02-01 13:18:56 +0200

    2.1 valmis

---
+++ kurssit/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Course = props => {
+  console.log(props)
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const Header = props =>
+  <h1>{props.course}</h1>
+
+
+const Content = props => (
+  <ul>
+    {props.parts.map(part => <Part key={part.id} part={part} />)}
+  </ul>
+)
+
+const Part = props =>
+  <p>{props.part.name} {props.part.exercises}</p>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
